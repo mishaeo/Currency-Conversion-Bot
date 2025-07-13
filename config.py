572dd2fc87c53b397c3302faa5918b985b9cb209
Mischa <150851766+mishaeo@users.noreply.github.com>
2025-07-13 09:38:47 +0200

    Commit - Updated file config

--- config.py
+++ config.py
@@ -1,11 +1,13 @@
 from dotenv import load_dotenv
 import os
 
-# Загружаем переменные из .env файла
 load_dotenv()
 
-# Получаем токен
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+APP_URL = os.getenv("APP_URL")
 
 if not BOT_TOKEN:
-    raise ValueError("BOT_TOKEN не найден в .env файле!")
+    raise ValueError("BOT_TOKEN not found!")
+
+if not APP_URL:
+    raise ValueError("APP_URL not found!")
